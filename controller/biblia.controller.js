928e26f1e9a320eedefc2ea98cb8e08b06a75797
Luciano Tassara <lucianotassara@gmail.com>
2020-09-04 00:16:52 -0300

    help endpoint

--- controller/biblia.controller.js
+++ controller/biblia.controller.js
@@ -5,7 +5,7 @@ import {BIBLE_BOOKS, bookNumber} from '../consts/libros'
 
 const bibliaController = express.Router();
 
-bibliaController.route('/').get(
+bibliaController.route('/help').get(
   (req, res) => {
     res.json({help: "You can make your request providing your own SQlite databases, make sure your database is correctly placed, check your .env file",
               message: `So.. Everything ready? Try this url-> http://{{host}}:${process.env.BIBLE_API_PORT}/146/JHN.3.16-18 - You can make your queries using the data listed below` ,
