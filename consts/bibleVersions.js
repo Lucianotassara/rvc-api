4a5fed3340fb5e28806b0ec7f9bc6e16bf542eb4
Luciano Tassara <lucianotassara@gmail.com>
2020-09-04 23:08:47 -0300

    fix codigo version

--- consts/bibleVersions.js
+++ consts/bibleVersions.js
@@ -46,22 +46,22 @@ export const BIBLE_VERSIONS = [
 export function getBibleVersion(n) {
     switch (n.toUpperCase()) {
         // Ingreso con version
-        case 'RVC': return { "ruta": "RVC.bblx", "id": "146"}
-        case 'DHH': return { "ruta": "DHH.bblx", "id": "411"}
-        case 'RV60': return { "ruta": "RV60.bblx", "id":"149"}
-        case 'TLA': return { "ruta": "TLA.bblx", "id": "176"}
-        case 'NVI': return { "ruta": "NVI.bblx", "id": "128"}
-        case 'RV95': return { "ruta": "RV95.bblx", "id":"150"}
-        case 'PDT': return { "ruta": "PDT.bblx", "id": "197"}
+        case 'RVC': return { "ruta": "RVC.bblx", "id": "146", "version": "RVC" }
+        case 'DHH': return { "ruta": "DHH.bblx", "id": "411", "version": "DHH" }
+        case 'RV60': return { "ruta": "RV60.bblx", "id":"149", "version": "RV60" }
+        case 'TLA': return { "ruta": "TLA.bblx", "id": "176", "version": "TLA" }
+        case 'NVI': return { "ruta": "NVI.bblx", "id": "128", "version": "NVI" }
+        case 'RV95': return { "ruta": "RV95.bblx", "id":"150", "version": "RV95" }
+        case 'PDT': return { "ruta": "PDT.bblx", "id": "197", "version": "PDT" }
         
         // Ingreso con id
-        case '146': return { "ruta": "RVC.bblx", "version": "RVC" }
-        case '411': return { "ruta": "DHH.bblx", "version": "DHH" }
-        case '149': return { "ruta": "RV60.bblx", "version": "RV60" }
-        case '176': return { "ruta": "TLA.bblx", "version": "TLA" }
-        case '128': return { "ruta": "NVI.bblx", "version": "NVI" }
-        case '150': return { "ruta": "RV95.bblx", "version": "RV95" }
-        case '197': return { "ruta": "PDT.bblx", "version": "PDT" }
+        case '146': return { "ruta": "RVC.bblx", "id": "146", "version": "RVC" }
+        case '411': return { "ruta": "DHH.bblx", "id": "411", "version": "DHH" }
+        case '149': return { "ruta": "RV60.bblx", "id":"149",  "version": "RV60" }
+        case '176': return { "ruta": "TLA.bblx", "id": "176", "version": "TLA" }
+        case '128': return { "ruta": "NVI.bblx", "id": "128", "version": "NVI" }
+        case '150': return { "ruta": "RV95.bblx", "id":"150",  "version": "RV95" }
+        case '197': return { "ruta": "PDT.bblx", "id": "197", "version": "PDT" }
         
         default: return {'error': true}
         // retornar default cuando no se reconoce la versión para que no falle.
